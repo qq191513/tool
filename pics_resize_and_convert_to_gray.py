9db8c0de838240381342268b37b5018503d9e698
ccav <1915138054@qq.com>
2018-11-04 16:41:27 +0800

    add

--- pics_resize_and_convert_to_gray.py
+++ pics_resize_and_convert_to_gray.py
@@ -2,11 +2,14 @@
 import os
 import cv2
 
-def create_new_route(file,size):
+def create_new_route(file,size,resize_pic,convert_to_gray):
     # work:传入一个文件的完整路径，返回新的目标路径和无后缀文件名
     file = file.split('/') #切开
     first_name = file[0]
-    first_name = first_name + '_{}x{}'.format(size,size)  #改名
+    if convert_to_gray:
+        first_name = first_name + '_gray'
+    if resize_pic:
+        first_name = first_name + '_{}x{}'.format(size,size)  #改名
     file[0] =first_name
     file_name = file[-1]  #取最后一个名字
     file_name = file_name.split('.')[0]  # 切开取第0个
@@ -25,11 +28,14 @@ def get_files_list(path):
             files_list.append(os.path.join(parent,filename))
     return files_list
 
-def resize_pic_and_save(file,size= 32):
-    new_route, file_name = create_new_route(file,size)
+def resize_pic_and_save(file,size= 32,resize_pic = True,convert_to_gray =True):
+    new_route, file_name = create_new_route(file,size,resize_pic,convert_to_gray)
     file_name = file_name + '.jpg'
     pic = cv2.imread(file)
-    pic = cv2.resize(pic, (size, size), interpolation=cv2.INTER_CUBIC)
+    if resize_pic:  #是否resize图片
+        pic = cv2.resize(pic, (size, size), interpolation=cv2.INTER_CUBIC)
+    if convert_to_gray: #是否转成灰度图
+        pic = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
     save_new_route = os.path.join(new_route,file_name)
     cv2.imwrite(save_new_route,pic)
     print(save_new_route)
@@ -37,6 +43,6 @@ def resize_pic_and_save(file,size= 32):
 path = r'asl_dataset'
 #获取所有图片文件
 files_list = get_files_list(path)
-#一个个resize图片
+#一个个resize图片和转成灰度图
 for file in files_list:
-    resize_pic_and_save(file,size= 32)
+    resize_pic_and_save(file,size= 32,resize_pic = True,convert_to_gray = True)
