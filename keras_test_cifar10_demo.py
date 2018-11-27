af79fc7126bb61ff9e920a5c7465572876de7805
ccav <1915138054@qq.com>
2018-11-27 20:03:31 +0800

    Update keras_test_cifar10_demo.py

--- keras_test_cifar10_demo.py
+++ keras_test_cifar10_demo.py
@@ -37,8 +37,7 @@ from keras.utils import np_utils
 from keras.utils.vis_utils import plot_model
 import tensorflow as tf
 from keras.backend.tensorflow_backend import set_session
-import keras.backend.tensorflow_backend as KTF
-import os
+
 
 #########################   使用GPU  动态申请显存占用 ####################
 # 1、使用allow_growth option，刚一开始分配少量的GPU容量，然后按需慢慢的增加，由于不会释放内存，所以会导致碎片
@@ -46,6 +45,8 @@ import os
 # 3、allow_soft_placement如果指定的设备不存在，允许TF自动分配设备（这个设置必须有，否则无论如何都会报cudnn不匹配的错误）
 # 4、per_process_gpu_memory_fraction  指定每个可用GPU上的显存分配比
 import tensorflow as tf
+import keras.backend.tensorflow_backend as KTF
+import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 session_config = tf.ConfigProto(
             device_count={'GPU': 0},
