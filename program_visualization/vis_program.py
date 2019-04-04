1b9bad60b429b1fc43510067c731fe2c08209292
c <1915138054@qq.com>
2019-04-04 16:30:52 +0800

    Update vis_program.py

--- program_visualization/vis_program.py
+++ program_visualization/vis_program.py
@@ -6,33 +6,47 @@ from pycallgraph import GlobbingFilter
 import cv2
 import os
 ##############################    改这里      ####################################
+myinclude = [
+'train_app.*',
+'train_mars.*',
+'vis_tools.*',
+'queued_trainer.*',
+'metrics.*',
+'losses.*',
+'network_definition.*',
+'residual_net.*',
+'mars.*',
+'util.*',
+]
 def vis_program():
     # 这里写要观察的主函数代码。
-    from logistic_regression import logistic
-    logistic()
+    from train_mars import main_fuck
+    main_fuck()
 #希望包含的函数
-# myinclude = [
-#     'main',
-#     'Activation',
-#     'MaxPooling2D',
-#     'Convolution2D'
-# ]
+
 ###############################   end      #######################################
 
 if __name__ == "__main__":
     config = Config()
     # 关系图中包括(include)哪些函数名。
     # 如果是某一类的函数，例如类gobang，则可以直接写'gobang.*'，表示以gobang.开头的所有函数。（利用正则表达式）。
-    # config.trace_filter = GlobbingFilter(include=myinclude)
+    # config.trace_filter = GlobbingFilter(include=myinclude),
     # 该段作用是关系图中不包括(exclude)哪些函数。(正则表达式规则)
-    config.trace_filter = GlobbingFilter(exclude=[
-        'pycallgraph.*',
-        '*.secret_function',
-        'FileFinder.*',
-        'ModuleLockManager.*',
-        'SourceFilLoader.*'
-    ])
-    # config.trace_filter = GlobbingFilter(include=myinclude)
+    #################   排除过滤     ########################
+    # config.trace_filter = GlobbingFilter(exclude=[
+    #     'pycallgraph.*',
+    #     '*.secret_function',
+    #     'FileFinder.*',
+    #     '_ModuleLockManager.*',
+    #     'SourceFilLoader.*',
+    # ],
+    # include=myinclude)
+    #################   end     ###########################
+
+    #################   包含过滤     ########################
+    config.trace_filter = GlobbingFilter(include=myinclude)
+    #################   end     ###########################
+
     graphviz = GraphvizOutput()
     save_name = 'graph.png'
     graphviz.output_file = save_name
